4d253b830f312041dff137ac87211eb9d3a9694c
davidemmolino <davidewebdev@gmail.com>
2020-10-12 21:32:01 -0700

    Updates

---
+++ precache-manifest.6384bb057f4e3f7ebe6af84b80865eb5.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea70c0ebac3acf7976a0ead3b9c7eb5b",
+    "url": "/recoilize-landing-page/index.html"
+  },
+  {
+    "revision": "bbece4f5cdc323bc0927",
+    "url": "/recoilize-landing-page/static/css/main.df368ee1.chunk.css"
+  },
+  {
+    "revision": "514af77d5cadc9f98f8e",
+    "url": "/recoilize-landing-page/static/js/2.61c91a67.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/recoilize-landing-page/static/js/2.61c91a67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bbece4f5cdc323bc0927",
+    "url": "/recoilize-landing-page/static/js/main.65c044b2.chunk.js"
+  },
+  {
+    "revision": "58ffc3f6aa8a5e5a562a",
+    "url": "/recoilize-landing-page/static/js/runtime-main.0a75c892.js"
+  },
+  {
+    "revision": "f002f2278f17076475a9f8d7fd527a2a",
+    "url": "/recoilize-landing-page/static/media/demo1.b9094b36.gif"
+  },
+  {
+    "revision": "1bb027109345a90a9eab1e929d8669c2",
+    "url": "/recoilize-landing-page/static/media/github-logo.1bb02710.svg"
+  },
+  {
+    "revision": "d68b043e5711dd63b1fb734fcfd66d47",
+    "url": "/recoilize-landing-page/static/media/interpolate.67945ad3.gif"
+  },
+  {
+    "revision": "8cc2d3a80c1ebec192c2f29c08333dc6",
+    "url": "/recoilize-landing-page/static/media/linkedin-logo.8cc2d3a8.svg"
+  },
+  {
+    "revision": "ea09813e5cbea292c48c47cf8e5e8f67",
+    "url": "/recoilize-landing-page/static/media/recoilize-gray-01.75757e59.png"
+  }
+]);
